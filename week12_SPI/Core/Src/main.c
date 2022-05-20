a6bcb7c7ed436250a31e6ece700fde1154402205
Owl_Hor <30950703+owlhor@users.noreply.github.com>
2022-05-20 17:49:59 +0700

    commit

--- week12_SPI/Core/Src/main.c
+++ week12_SPI/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "math.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
